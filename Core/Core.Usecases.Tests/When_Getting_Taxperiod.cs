21efe22ef1dfc26c0241362081fa2d6055153fda
AHP <anders@bjergtoppen.dk>
2020-10-07 14:30:14 +0200

    Update test of When_Getting_Taxperiod

--- Core/Core.Usecases.Tests/When_Getting_Taxperiod.cs
+++ Core/Core.Usecases.Tests/When_Getting_Taxperiod.cs
@@ -7,20 +7,25 @@ namespace Core.Usecases.Tests
 {
     public class When_Getting_Taxperiod
     {
-        [Fact]
-        public async void Should_Return_Shortest_Period_That_Spans_Given_Date()
+        //Name, date, expectedTaxPercentage
+        [Theory]
+        [InlineData("Copenhagen", "2016/01/01", 10)]
+        [InlineData("Copenhagen", "2016/01/02", 20)]
+        [InlineData("Copenhagen", "2016/05/01", 40)]
+        [InlineData("Copenhagen", "2016/05/31", 40)]
+        [InlineData("Copenhagen", "2016/07/10", 20)]
+        public async void Should_Return_Shortest_Period_That_Spans_Given_Date(string name, string datestring, int expectedTax)
         {
             //Arrange
             var repo = new FakeRepository();
             var handler = new MunicipalityHandler(repo);
-            var name = "Copenhagen";
-            var date = new DateTime(2016, 1, 1);
+            var date = DateTime.Parse(datestring);
             
             //Act
             var res = await handler.GetTaxInMunicipalityForGivenDate(name, date);
 
             //Assert
-            res.TaxPercentage.ShouldBe(10);
+            res.TaxPercentage.ShouldBe(expectedTax);
         }
     }
 }
